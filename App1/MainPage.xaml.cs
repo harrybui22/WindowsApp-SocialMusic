1ba84bfe993bd4e48fa83e0317ee63e65a3cdf97
Harry <harrybui22@gmail.com>
2013-11-09 13:09:49 -0800

    json from wiki

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Windows.Web.Http;
+using Windows.Web.Http.Headers;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +26,39 @@ namespace App1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private HttpClient httpClient;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            httpClient = new HttpClient();
+
+            var headers = httpClient.DefaultRequestHeaders;
+            var downloaded = getData();
+
+
+
+        }
+
+        private async Task<string> getData() {
+ 
+            string content = await httpClient.GetStringAsync(
+                new Uri("http://en.wikipedia.org/w/api.php?format=json&action=query&titles=Avicii&prop=revisions&rvprop=content"));
+            
+            return content;
+        }
+
+        async private void ReadDataFromWiki()
+        {
+            var client = new HttpClient();
+            
+
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
